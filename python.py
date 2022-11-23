3ed4d7a7fe3bf562c005cd7aa349eb5b576e8777
awanijain27 <113438380+awanijain27@users.noreply.github.com>
2022-11-23 11:12:41 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,49 +2,50 @@ from array import *
 import numpy 
 import readchar
 import os
+import keyboard
 import time
-import pygame
 
-playfield = numpy.chararray(shape=(15,40))
+global playfield, food_collect, game_end, py, px, gy1, gx1, gy2, gx2
+
 playfield = [
-    ["****************************************"],
-    ["* .....................................*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*......................................*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*..*..*..*..*..*..*..*..*..*..*..*..*..*"],
-    ["*......................................*"],
-    ["****************************************"]
+    "****************************************",
+    "* .....................................*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*......................................*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*..*..*..*..*..*..*..*..*..*..*..*..*..*",
+    "*......................................*",
+    "****************************************"
 ]
+food_collect = 0
+game_end = 0
+py = 1   #these variables represent pacman coordinate
+px = 1   
+gy1 = 1   #these variables represent ghosts coordinate
+gx1 = 38
+gy2 = 13
+gx2 = 1  
+
 
 def draw_playfield():
-    print("\033[0;31m")
-    print("\n\n\n\n")
-    for i in range(0, 15):
+    os.system("cls")
+    for i in range(0, 14):
         print("\t\t")
-        for j in range(0,40):
-            print(playfield[i][j])
-        print("\n")
+        for j in range(0,39):
+            print(playfield[i][j],end="")
 
-food_collect=0
-game_end=0
-py=1   #these variables represent pacman coordinate
-px=1   
-gy1=1   #these variables represent ghosts coordinate
-gx1=38
-gy2=13
-gx2=1   
+def replace_char(str,index,new):
+    return str[:index] + new + str[index+1:]
 
 def game_win():
-    readchar
-    os.system('clear')
+    os.system('cls')
     if(food_collect==500):
         print("\n\n\n\n\n\n\n\n\n")
         print("\t\t\t        Congratulation!\n")
@@ -52,6 +53,7 @@ def game_win():
         print("\t\t\t     Your total score is"+food_collect)
 
 def game_lose():
+    os.system('cls')
     if(food_collect<500):
         print("\n\n\n\n\n\n\n\n\n")
         print("\t\t\t          Better luck!\n")
@@ -59,6 +61,7 @@ def game_lose():
         print("\t\t\t     Your total score is",+food_collect)
 
 def move_ghost1():
+    global gx1,gy1
     if(gy1==1 and gx1 > 1):
         gx1-=1 #this statement move 1st ghost coordinate to left side
     
@@ -72,6 +75,7 @@ def move_ghost1():
         gy1-=1 #this statement move 1st ghost coordinate to up side
 
 def move_ghost2():
+    global gx2,gy2
     if(gy2 == 13 and gx2 < 38):
         gx2+=1 #this statement move 2nd ghost coordinate to right side
 
@@ -85,24 +89,26 @@ def move_ghost2():
         gy2+=1 #this statement move 2nd ghost coordinate to down side
 
 def user_inputs():
-    for playfield in pygame.playfield.get():
-        if(kbhit()):
-            if(playfield.type == pygame.KEYDOWN):
-                py+=1
-            elif(playfield.type == pygame.K_RIGHT):
-                px+=1
-            elif(playfield.type == pygame.K_LEFT):
-                px-=1
-            elif(playfield.type == pygame.KEYUP):
-                py-+1
+    global px,py
+    if(keyboard.is_pressed('w')):
+        py-=1
+    elif(keyboard.is_pressed('d')):
+        px+=1
+
+    elif(keyboard.is_pressed('a')):
+        px-=1
+    if(keyboard.is_pressed('s')):
+        py+=1
+
 
 def setup():
-    for i in range(0, 15):
-        for j in range(0,40):
+    global px,py,food_collect,game_end
+    for i in range(0, 14):
+        for j in range(0,39):
             if(playfield[i][j]=='#'):
-                playfield[i][j]=' '
+                playfield[i]=replace_char(playfield[i], j, '')
             elif(playfield[i][j]=='@'):
-                playfield[i][j]='.'
+                playfield[i]=replace_char(playfield[i], j, '.')
       
     if(playfield[py][px]=='.'):
         food_collect+=1
@@ -110,41 +116,36 @@ def setup():
         py=1
         px=1
     
-    playfield[py][px]='#'
-    playfield[gy1][gx1]='@'
-    playfield[gy2][gx2]='@'
-
-    if(playfield[py][px]=='@'):
-        game_end=1
+    playfield[py]=replace_char(playfield[py],px,'#')
+    playfield[gy1]=replace_char(playfield[gy1],gx1,'@')
+    playfield[gy2]=replace_char(playfield[gy2],gx2,'@')
 
 def delay():
     time.sleep(0.1)
 
 def game_lost():
+    global game_end
     if(playfield[py][px]=='@'):
         game_end=1
 
 i=100
+temp=True
 while(game_end!=1):
-    readchar
-    os.system(ls)
+    if(temp==True):
+        char=readchar.readchar()
+    user_inputs()
     setup()
-    user_input()
     move_ghost1()
     move_ghost2()
     draw_playfield()
     game_lost()
-    delay(0.1)
+    delay()
     if(i < 1000):
         i=i+100
     else:
         i=100
+    temp = False
 
 game_win()
 game_lose()
-
-
-
-
-
 
